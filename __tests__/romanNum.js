10128af0613b1f0ade54fcc3b97c83fdffa62a6d
Rithika Ganesna <ganesnar@oregonstate.edu>
2024-05-01 09:58:43 -0700

    commit 4: RED PHASE-> tests failed because the expected was L for both 1000 and 2000. it received M and MM respectively, so I know the converter works.

--- __tests__/romanNum.js
+++ __tests__/romanNum.js
@@ -1,13 +1,22 @@
 const romanNum = require("../lib/romanNum")
 
-//should fail, because 1 is a valid number
+
 test("number below 1 is an invalid input", function(){
     const converter = new romanNum();
     expect(() => romanNum(0)).toThrow();
 })
 
-//should fail, because 3999 is a valid number
 test("Number greater that 3999 is an invalid input", function(){
     const converter = new romanNum();
     expect(() => romanNum(4000)).toThrow();
+})
+
+//this should fail, making sure it fails because 1000 should be M
+test("any multiple of 1000 gets converted to M", function(){
+    expect(romanNum(1000)).toBe("L");
+})
+
+//should also fail
+test("2000 gets converted to MM", function(){
+    expect(romanNum(2000)).toBe("L");
 })
