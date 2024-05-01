74f928203b1b3060ef1b5dbb833ff6b93aced783
Rithika Ganesna <ganesnar@oregonstate.edu>
2024-05-01 09:52:27 -0700

    commit 3: GREEN PHASE tests passed! this means the error handling works

--- __tests__/romanNum.js
+++ __tests__/romanNum.js
@@ -3,11 +3,11 @@ const romanNum = require("../lib/romanNum")
 //should fail, because 1 is a valid number
 test("number below 1 is an invalid input", function(){
     const converter = new romanNum();
-    expect(() => romanNum(1)).toThrow();
+    expect(() => romanNum(0)).toThrow();
 })
 
 //should fail, because 3999 is a valid number
 test("Number greater that 3999 is an invalid input", function(){
     const converter = new romanNum();
-    expect(() => romanNum(3999)).toThrow();
+    expect(() => romanNum(4000)).toThrow();
 })
